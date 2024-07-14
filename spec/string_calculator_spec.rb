bd3ef7baec5759079cd4857097ac240f7aef31f4
Suyog <suyog.taksande@gmail.com>
2024-07-14 06:06:11 +0000

    spec for returning sum of string integers.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,5 +8,10 @@ RSpec.describe StringCalculator do
       puts "calculator.add: #{calculator.add("")}"
       expect(calculator.add("")).to eq(0)
     end
+
+    it 'should return sum for multiple string integers' do
+      string = "1,2,6,10"
+      expect(calculator.add(string)).to eq(string.split(",").map(&:to_i).sum)
+    end
   end
 end
